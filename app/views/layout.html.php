9fe2e59f3f44a17d7b7cf04021732869a0ddb360
abr4xas <bullgram@gmail.com>
2013-08-13 11:45:37 -0430

    actualizacion

--- app/views/layout.html.php
+++ app/views/layout.html.php
@@ -22,16 +22,12 @@
 
 	<aside>
 
-		<h1><a href="<?php echo site_url() ?>"><?php echo config('blog.title') ?></a></h1>
+		<h2><a href="<?php echo site_url() ?>"><?php echo config('blog.title') ?></a></h2>
 
 		<p class="description"><?php echo config('blog.description')?></p>
 		<!-- the sidebar-->
 		<ul>
 			<li><a href="<?php echo site_url() ?>">Home</a></li>
-			<li><a href="https://github.com/SiteOctopus/Light-Blog/zipball/master">Zip download</a></li>
-			<li><a href="https://github.com/SiteOctopus/Light-Blog/tarball/master">tar.gz download</a></li>
-			<li><a href="https://github.com/SiteOctopus/Light-Blog">Github Page</a></li>
-			<li><a href="http://siteoctopus.github.com/Light-Blog/">Our Site</a></li>
 		</ul>
 		<!-- End custom sidebar-->
 		<p class="author"><?php echo config('blog.authorbio') ?></p>
